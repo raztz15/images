fc2b8cd07b28279819bcfb0e28a223e1f268c6af
unknown <raztz15@gmail.com>
2022-04-03 15:46:52 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import CuralifeApp from './Components/Curalife-app'
+
+function App() {
+  return (
+    <div className="App">
+      <CuralifeApp/>
+      
+    </div>
+  );
+}
+
+export default App;
